6c4b826dd39a16a83dd9b961d667aacde6c92d11
Brittney Chin <33679441+brittchin99@users.noreply.github.com>
2020-04-17 18:08:25 -0400

    Add files via upload
    section 1 starter files

---
+++ starter-code/mad_libs_starter.py
@@ -0,0 +1,8 @@
+#PROJECT: Mad Libs
+#INSTRUCTIONS - create a Mad Libs story where a user will enter multiple words
+#which will then be placed into a story
+
+#define the variables you want to use in your story
+
+
+#print your story here
---
+++ starter-code/variable_practice_answers.py
@@ -0,0 +1,20 @@
+#SECTION 1: Variable Practice answers
+#INSTRUCTIONS - write your code below each commented line
+
+#define a variable called myVar and set it equal to a whole number, then print the value
+myVar = 6
+print(myVar)
+
+#assign the same variable to a new float number, then print its new value
+myVar = 3.14
+print(myVar)
+
+#assign the variable to a string, then print its new value
+myVar = "Hello!"
+print(myVar)
+
+#finally, assign the variable to a boolean, and print its value
+myVar = True
+print(myVar)
+
+#save the file and then run your program (run, run module)
