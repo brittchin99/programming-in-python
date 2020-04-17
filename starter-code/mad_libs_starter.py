a78fa7b6f32c3936a75b57d9d4818ac376c07e55
Brittney Chin <33679441+brittchin99@users.noreply.github.com>
2020-04-17 18:12:51 -0400

    Delete mad_libs_starter.py

--- starter-code/mad_libs_starter.py
+++
@@ -1,8 +0,0 @@
-#PROJECT: Mad Libs
-#INSTRUCTIONS - create a Mad Libs story where a user will enter multiple words
-#which will then be placed into a story
-
-#define the variables you want to use in your story
-
-
-#print your story here
